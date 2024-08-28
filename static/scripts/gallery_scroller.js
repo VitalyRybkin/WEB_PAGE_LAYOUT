e12bed2dcc3c42c786dda0033b0be2ec71e308ff
vitaly <vitaly.rybkin@gmail.com>
2024-08-28 11:28:31 +0300

    fixed: two galleries refactored

--- static/scripts/gallery_scroller.js
+++ static/scripts/gallery_scroller.js
@@ -1,45 +1,46 @@
-const dotElems = Array.from(document.querySelectorAll(".comments__gallery_dot"));
+let index;
 
-let index = 0;
+let galleryStyles;
+let cardWidth;
 
-const commentCards = document.querySelectorAll(".comments__gallery_block");
-const sliderLine = document.querySelector(".comments__gallery_container");
+function init(block) {
+    index = 0;
+    const dotElems = Array.from(document.querySelectorAll(`.${block} .gallery_dot`));
+    const commentCards = document.querySelectorAll(`.${block} .gallery_block`);
+    const cardWidth = commentCards[0].offsetWidth;
+    const sliderLine = document.querySelector(`.${block} .gallery_container`);
+    galleryStyles = getComputedStyle(sliderLine);
 
-let cardWidth = commentCards[0].offsetWidth;
-
-const galleryStyles = getComputedStyle(document.querySelector(".comments__gallery_container"));
-
-function init() {
-    const galleryWidth = document.querySelector(".comments__gallery").offsetWidth;
-
+    const galleryWidth = document.querySelector(`.${block} .gallery`).offsetWidth;
     commentCards[0].style.marginLeft = galleryWidth / 2 - parseInt(galleryStyles.paddingLeft) - cardWidth / 2 + "px";
-    rollSlider();
+    rollSlider(sliderLine, cardWidth);
+
+    dotElems.forEach((elem) => {
+        elem.addEventListener("click", (e) => {
+            index = dotElems.indexOf(e.target);
+            rollSlider(sliderLine, cardWidth);
+
+            dotElems.forEach((elem) => {
+                if (elem.classList.contains("active")) {
+                    elem.classList.remove("active");
+
+                    commentCards.forEach((card) => {
+                        card.classList.remove("shown");
+                    })
+                }
+            });
+
+            commentCards[dotElems.indexOf(elem)].classList.add("shown");
+            e.target.classList.add("active")
+        })
+    })
 }
 
-dotElems.forEach((elem) => {
-    elem.addEventListener("click", (e) => {
-        index = dotElems.indexOf(e.target);
-        rollSlider();
 
-        dotElems.forEach((elem) => {
-            if (elem.classList.contains("active")) {
-                elem.classList.remove("active");
-
-                commentCards.forEach((card) => {
-                    card.classList.remove("shown");
-                })
-            }
-        });
-
-        commentCards[dotElems.indexOf(elem)].classList.add("shown");
-        e.target.classList.add("active")
-    })
-})
-
-function rollSlider() {
+function rollSlider(sliderLine, cardWidth) {
     sliderLine.style.transform = 'translate(-' + (index * cardWidth + index * parseInt(galleryStyles.gap)) + 'px)';
-
 }
 
 window.addEventListener("resize", init);
-init()
+init('comments')
+init('news')
